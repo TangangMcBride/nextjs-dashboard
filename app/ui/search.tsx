95764cb6a32d9d05b7a39bf6a2a6be86a40d7c7f
TangangMcBride <tangang.mcbride@ictuniversity.edu.cm>
2024-03-14 16:25:28 +0100

    Addd search and pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,24 +1,26 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter   } from 'next/navigation';
-
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
   const searchParams = useSearchParams();
-  function handleSearch(term: string){
-    const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }{
+
+    const handleSearch = useDebouncedCallback((term) => {
+      const params = new URLSearchParams(searchParams);
+      params.set('page', '1');
+      if (term) {
+        params.set('query', term);
+      } else {
+        params.delete('query');
+      }
+
       replace(`${pathname}?${params.toString()}`);
-    }
-   
-  }
+    }, 300);
+  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -27,7 +29,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e)=>{
+        onChange={(e) => {
           handleSearch(e.target.value);
         }}
         defaultValue={searchParams.get('query')?.toString()}
